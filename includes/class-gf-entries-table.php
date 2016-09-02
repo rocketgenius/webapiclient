124e17a9f8cbabcac3e1840d9f965f860e9bbd27
Steve Henty <stevehenty@gmail.com>
2016-09-02 20:08:51 +0200

    support for v2

--- includes/class-gf-entries-table.php
+++ includes/class-gf-entries-table.php
@@ -1,52 +1,75 @@
 <?php
 
-if (!class_exists('WP_List_Table'))
-    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
 
 class GFEntriesTable extends WP_List_Table {
 
-    private $_entries;
+	private $_entries;
 
-    function __construct($entries) {
-        $this->_entries = $entries;
+	function __construct( $entries ) {
+		$this->_entries = $entries;
 
-        $this->_column_headers = array(
-            array("id" => "Id", "date_created" => "Date", "is_read" => "Read", "is_starred" => "Starred", "status"=> "Status"),
-            array(),
-            array("id" => array("id", true), "date_created" => array("date_created", true), "is_read" => array("is_read", false), "is_starred" => array("is_starred", false), "status"=> array("status", false))
-        );
+		$this->_column_headers = array(
+			array(
+				'id'           => 'Id',
+				'date_created' => 'Date',
+				'is_read'      => 'Read',
+				'is_starred'   => 'Starred',
+				'status'       => 'Status',
+			),
+			array(),
+			array(
+				'id'           => array( 'id', true ),
+				'date_created' => array( 'date_created', true ),
+				'is_read'      => array( 'is_read', false ),
+				'is_starred'   => array( 'is_starred', false ),
+				'status'       => array( 'status', false ),
+			),
+		);
 
-        parent::__construct(array(
-            'singular' => __('entry', 'gravityforms'),
-            'plural'   => __('entries', 'gravityforms'),
-            'ajax'     => false
-        ));
-    }
+		parent::__construct( array(
+			'singular' => __( 'entry', 'gravityforms' ),
+			'plural'   => __( 'entries', 'gravityforms' ),
+			'ajax'     => false,
+		) );
+	}
 
-    function prepare_items() {
-        $this->items = isset($this->_entries) ? $this->_entries : array();
-    }
+	function get_columns() {
+		return array(
+			'id'           => 'Id',
+			'date_created' => 'Date',
+			'is_read'      => 'Read',
+			'is_starred'   => 'Starred',
+			'status'       => 'Status',
+		);
+	}
 
-    function no_items() {
-        echo "This form doesn't have any entries";
-    }
+	function prepare_items() {
+		$this->items = isset( $this->_entries ) ? $this->_entries : array();
+	}
 
-    function column_default($item, $column) {
+	function no_items() {
+		echo "This form doesn't have any entries";
+	}
 
-        return rgar($item, $column);
-    }
+	function column_default( $item, $column ) {
 
-    function column_id($item){
+		return rgar( $item, $column );
+	}
 
-        $actions = array("edit" => "<a title='Edit Entry' href=' " . add_query_arg(array("entry_id" => $item["id"])) . "'>Edit</a>",
-                         "delete" => "<a title='Delete' onclick='DeleteEntry(" . $item["id"] . ")' href='javascript:void(0);'>Delete</a>"
-                        );
-        return rgar($item, "id") . " " . $this->row_actions($actions);
-    }
+	function column_id( $item ) {
 
-    function column_cb($item) {
-        return "";
-    }
+		$actions = array(
+			'edit'   => "<a title='Edit Entry' href=' " . add_query_arg( array( 'entry_id' => $item['id'] ) ) . "'>Edit</a>",
+			'delete' => "<a title='Delete' onclick='DeleteEntry(" . $item["id"] . ")' href='javascript:void(0);'>Delete</a>",
+		);
 
+		return rgar( $item, 'id' ) . ' ' . $this->row_actions( $actions );
+	}
 
+	function column_cb( $item ) {
+		return '';
+	}
 }
--- includes/class-gf-forms-table.php
+++ includes/class-gf-forms-table.php
@@ -1,54 +1,65 @@
 <?php
 
-if (!class_exists('WP_List_Table'))
-    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+if ( ! class_exists( 'WP_List_Table' ) ) {
+	require_once( ABSPATH . 'wp-admin/includes/class-wp-list-table.php' );
+}
 
 class GFFormsTable extends WP_List_Table {
 
-    private $_forms;
+	private $_forms;
 
-    function __construct($forms) {
-        $this->_forms = $forms;
+	function __construct( $forms ) {
+		$this->_forms = $forms;
 
-        $this->_column_headers = array(
-            array("title" => "Title", "id" => "Id", "entries"=>"Entries"),
-            array(),
-            array()
-        );
+		$this->_column_headers = array(
+			array( 'title' => 'Title', 'id' => 'Id', 'entries' => 'Entries' ),
+			array(),
+			array(),
+		);
 
-        parent::__construct(array(
-            'singular' => __('form', 'gravityforms'),
-            'plural'   => __('forms', 'gravityforms'),
-            'ajax'     => false
-        ));
-    }
+		parent::__construct( array(
+			'singular' => __( 'form', 'gravityforms' ),
+			'plural'   => __( 'forms', 'gravityforms' ),
+			'ajax'     => false,
+		) );
+	}
 
-    function prepare_items() {
-        $this->items = isset($this->_forms) ? $this->_forms : array();
-    }
+	function get_columns() {
+		return array( 'title' => 'Title', 'id' => 'Id', 'entries' => 'Entries' );
+	}
 
-    function no_items() {
-        echo "You don't have any forms";
-    }
+	function prepare_items() {
+		$this->items = isset( $this->_forms ) ? $this->_forms : array();
+	}
 
-    function column_default($item, $column) {
+	function no_items() {
+		echo "You don't have any forms";
+	}
 
-        return rgar($item, $column);
-    }
+	function column_default( $item, $column ) {
 
-    function column_title($item){
+		return rgar( $item, $column );
+	}
 
-        $actions = array("edit" => "<a title='Edit Form' href=' " . add_query_arg(array("form_id" => $item["id"])) . "'>Edit</a>",
-                         "entries" => "<a title='View Entries' href=' " . add_query_arg(array("form_id" => $item["id"], "view" => "entries")) . "'>Entries</a>",
-                         "results" => "<a title='View Results' href=' " . add_query_arg(array("form_id" => $item["id"], "view" => "results")) . "'>Results</a>",
-                         "delete" => "<a title='Delete' onclick='DeleteForm(" . $item["id"] . ")' href='javascript:void(0);'>Delete</a>"
-                        );
-        return rgar($item, "title") . " " . $this->row_actions($actions);
-    }
+	function column_title( $item ) {
 
-    function column_cb($item) {
-        return "";
-    }
+		$actions = array(
+			'edit'    => "<a title='Edit Form' href=' " . add_query_arg( array( 'form_id' => $item['id'] ) ) . "'>Edit</a>",
+			'entries' => "<a title='View Entries' href=' " . add_query_arg( array(
+					'form_id' => $item['id'],
+					'view'    => 'entries',
+				) ) . "'>Entries</a>",
+			'results' => "<a title='View Results' href=' " . add_query_arg( array(
+					'form_id' => $item['id'],
+					'view'    => 'results'
+				) ) . "'>Results</a>",
+			'delete'  => "<a title='Delete' onclick='DeleteForm(" . $item["id"] . ")' href='javascript:void(0);'>Delete</a>",
+		);
 
+		return rgar( $item, "title" ) . " " . $this->row_actions( $actions );
+	}
 
+	function column_cb( $item ) {
+		return '';
+	}
 }
